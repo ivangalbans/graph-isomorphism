3c4ed23599b161ba96b90184073d2e19d1292e5f
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2018-05-16 19:52:58 -0400

    finish isomorphism implementation

--- Graph.Isomorphism.hs
+++ Graph.Isomorphism.hs
@@ -3,32 +3,37 @@ import Data.List
 
 data Graph v = G [v] (v -> [v])
 
-check :: (Int -> Int) -> Graph Int -> Graph Int -> Bool
+check :: (Eq a, Eq b) => (a -> b) -> Graph a -> Graph b -> Bool
 check f g1 g2 = and [map f (adj1 vi) == adj2 (f vi) | vi <- v1]
     where   G v1 adj1 = g1
             G v2 adj2 = g2
 
-isomorphism :: Graph Int -> Graph Int -> Bool
+isomorphism :: (Eq a, Eq b) => Graph a -> Graph b -> Bool
 isomorphism g1 g2 = or [ (check f  g1 g2) | vp <- permutations v1 , let f = function vp v2]
     where   G v1 adj1 = g1
             G v2 adj2 = g2
 
 
-function :: [Int] -> [Int] -> (Int -> Int)
-function (x:xs)  (y:ys) a   | x == a    = y
-                            | otherwise = function xs ys a
+function :: (Eq a, Eq b) => [a] -> [b] -> (a -> b)
+function (x:xs)  (y:ys) val     | x == val  = y
+                                | otherwise = function xs ys val
+
+
 
 ej_grafo1 = G [1..5] suc
-    where   suc 1 = [2,3]
+    where   suc 1 = [2, 3]
             suc 2 = [4]
             suc 3 = [4]
             suc _ = []
 
-ej_grafo2 = G [2..6] suc
-    where   suc 2 = [3, 4]
-            suc 3 = [5]
-            suc 4 = [5]
+
+data Vertice = A | B | C | D | E deriving (Show, Eq, Enum)
+ej_grafo2 = G [A .. E] suc
+    where   suc A = [B, C]
+            suc B = [D]
+            suc C = [D]
             suc _ = []
+
 
 main = do {
     putStrLn ( show (isomorphism ej_grafo1 ej_grafo2) );
--- Graph.hs
+++
@@ -1,6 +0,0 @@
-module Graph where
-
--- This library was developed to work with algorithm about graph
--- It have a implementation of type Graph.
-
-data Graph v = G [v] (v -> [v])
